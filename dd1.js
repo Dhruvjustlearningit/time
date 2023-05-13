55fe132d08475b37e698eeb550a1cead42a3f95f
Dhruv <133504804+Dhruvjustlearningit@users.noreply.github.com>
2023-05-13 17:50:20 +0530

    Delete dd1.js

--- dd1.js
+++
@@ -1,9 +0,0 @@
-function changeLanguage() {
-    var select = document.getElementById('language-selector');
-    var value = select.options[select.selectedIndex].value;
-    document.cookie = "language=" + value + ";path=/";
-    window.location.reload();
-}
-
-var select = document.getElementById("language-selector");
-select.addEventListener("change", changeLanguage);
